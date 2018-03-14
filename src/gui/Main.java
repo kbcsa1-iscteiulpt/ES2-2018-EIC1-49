fb045929484e28b2a1b3e2aba75f7b1619080c24
Gustavo Morais <gustavo.t.morais@gmail.com>
2018-03-14 19:05:02 +0000

    wip

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,9 +6,9 @@ import javax.mail.internet.AddressException;
 public class Main {
 
 	public static void main(String[] args) {
-		Support sup = new Support();
+		Support support = new Support();
 		try {
-			sup.SendEmail("projetodees7", "ogustavoegay", "ricardojoaosantos0@gmail.com", "", "test 4", "e agora");
+			support.SendEmail("projetodees7", "ogustavoegay", "ricardojoaosantos0@gmail.com", "", "test 4", "e agora");
 //			sup.sendEmail("ricardo.santos@letracinetica.pt", "ricardojoaosantos0@gmail.com", "test 4", "trabalha oh puta");
 			System.out.println("donezo");
 		} catch (AddressException e) {
