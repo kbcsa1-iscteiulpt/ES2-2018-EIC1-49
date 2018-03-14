d9a43274e3f422bc6df3b3769f7fa63f766109ec
kevin <kevin@DESKTOP-8JCH1KM.lan>
2018-03-14 22:48:21 +0000

    XML
    Start of xml editor.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,10 +6,10 @@ import javax.mail.internet.AddressException;
 public class Main {
 
 	public static void main(String[] args) {
-		Support support = new Support();
+/*		Support support = new Support();
 		try {
 			support.SendEmail("projetodees7", "ogustavoegay", "ricardojoaosantos0@gmail.com", "", "test 4", "e agora");
-//			sup.sendEmail("ricardo.santos@letracinetica.pt", "ricardojoaosantos0@gmail.com", "test 4", "trabalha oh puta");
+			sup.sendEmail("ricardo.santos@letracinetica.pt", "ricardojoaosantos0@gmail.com", "test 4", "trabalha oh puta");
 			System.out.println("donezo");
 		} catch (AddressException e) {
 			// TODO Auto-generated catch block
@@ -18,7 +18,8 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		new Interface();
+	*/
+		new Interface();
 
 	}
 
