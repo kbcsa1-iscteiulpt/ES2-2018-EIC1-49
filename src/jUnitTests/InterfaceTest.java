abe93c6d8d00ff646ffd27b7ff1c20ead1576000
Unknown <dmcls@iscte-iul.pt>
2018-03-17 17:44:40 +0000

    JUnit tests for the interface are done. Smalls changes in the class Interface

---
+++ src/jUnitTests/InterfaceTest.java
@@ -0,0 +1,43 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JButton;
+
+import org.junit.Test;
+
+import gui.Interface;
+
+public class InterfaceTest {
+
+	private Interface interfaceTest;
+	@Test
+	public void inicialInterface() {
+		interfaceTest = new Interface();
+		
+		JButton helpButton = interfaceTest.getHelpButton();
+		helpButton.doClick();
+		
+		JButton emailButton = interfaceTest.getEmailButton();
+		emailButton.doClick();
+
+		JButton decisionVarButton = interfaceTest.getDecisionVarButton();
+		decisionVarButton.doClick();
+		
+		JButton readButton =interfaceTest.getReadButton();
+		readButton.doClick();
+		
+		JButton saveButton = interfaceTest.getSaveButton();
+		saveButton.doClick();
+		
+		JButton criterionButton = interfaceTest.getCriterionButton();
+		criterionButton.doClick();
+		
+		JButton addCriterionButton = interfaceTest.getAddCriterionButton();
+		addCriterionButton.doClick();
+		
+		JButton readJarButton = interfaceTest.getReadJarButton();
+		readJarButton.doClick();
+	}
+
+}
