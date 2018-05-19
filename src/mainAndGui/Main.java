3d726b90e1bdea6ac5eed96a7394f3a3a1221bf0
kevin <kbcsa1@iscte-iul.pt>
2018-05-20 00:09:44 +0100

    Main fix

--- src/mainAndGui/Main.java
+++ src/mainAndGui/Main.java
@@ -17,26 +17,10 @@ import support.XMLEditor;
 public class Main {
 
 	public static void main(String[] args) {
-	//	Config config = new Config();
-		//new Interface(config.getEmailAdmin());
+		Config config = new Config();
+		new Interface(config.getEmailAdmin());
 		
-		List<Variable> l = new ArrayList<Variable>();
-		Variable v1= new Variable("Variable 1","-1","5","0");
-		Variable v2= new Variable("Variable 2","0.5","2.5","0;1.0");
-		l.add(v1);l.add(v2);
-		
-		UserProblem problem = new UserProblem("problemName","problemDescription","email",Type.DOUBLE,
-				new Time(2,2,2),
-				new Time(3,3,3),
-				"decisionVariablesGroupName",2,l);
-		
-		Graphic chart = new Graphic(problem);
-		chart.readResults("./src/jUnitTests/graphic.rs");
-		chart.setContent();
-		chart.pack();
-		RefineryUtilities.centerFrameOnScreen(chart);
-		chart.setVisible(true);
-		fail("Not yet implemented");
+	
 	}
 
 }
