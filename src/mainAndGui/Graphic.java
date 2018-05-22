6b213b81bf397962b05bbabbc7d1f4e14b8a048e
Unknown <dmcls@iscte-iul.pt>
2018-05-22 14:33:00 +0100

    Graphics updated

--- src/mainAndGui/Graphic.java
+++ src/mainAndGui/Graphic.java
@@ -10,9 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.apache.commons.io.FileUtils;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
 import problem.UserProblem;
 import support.Config;
@@ -21,121 +26,154 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
- * This class creates a frame that shows a graphic with the values of the rf and rs files that contains
- * the JMetal results. 
+ * This class creates a frame that shows a graphic with the values of the rf and
+ * rs files that contains the JMetal results.
+ * 
  * @author Kevin Corrales nº 73529
  **/
-public class Graphic extends  ApplicationFrame {
-		
-	private final double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2;
-	private final double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2;
-	
+public class Graphic {
+
+	private final double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2;
+	private final double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2;
+
 	private UserProblem problem;
 	private List<double[]> xAxis = new ArrayList<double[]>();
 	private Config config = Config.getInstance();
 	
-	private String rfPath="";
-	private String rsPath="";
+	private String rsPath;
+	private String rfPath;
+	private List<String> algorithmsSelectedList = new ArrayList<String>();
 
-	
-		public Graphic(UserProblem problem) {
-		      super("");
-		      this.problem=problem;
-		      
-		      switch(problem.getType()) {
-		      	case DOUBLE:
-		      		rfPath=config.getrfPathDouble();
-		      		rsPath=config.getrsPathDouble();
-		      		break;
-		      	case BINARY:
-		      		rfPath=config.getrfPathBinary();
-		      		rsPath=config.getrsPathBinary();
-		      		break;
-		      	case INTEGER:
-		      		rfPath=config.getrfPathInteger();
-		      		rsPath=config.getrsPathInteger();
-		      		break;
-		      }
-		      
-		    readResults(rfPath);
-		    readResults(rsPath);
-		    setContent();
-		      
-		   }
-		
-			/**
-			 * This method sets the panel content of the graphic with the results 
-			 */
-			public void setContent() {
-				JFreeChart lineChart = ChartFactory.createLineChart(
-				         "JMetal Results",
-				         "","",
-				         createDataset(problem),
-				         PlotOrientation.VERTICAL,
-				         true,true,false);
-				         
-				      ChartPanel chartPanel = new ChartPanel( lineChart );
-				      chartPanel.setPreferredSize( new java.awt.Dimension((int) width ,(int) height) );
-				      setContentPane( chartPanel );
-			}
+	public Graphic(UserProblem problem, List<String> algorithmsSelectedList) {
+		this.problem = problem;
+		this.algorithmsSelectedList = algorithmsSelectedList;
 
+		switch(problem.getType()) {
+			case DOUBLE:
+				rsPath=config.getInstance().getrsPathDouble();
+				rfPath=config.getInstance().getrfPathDouble();
+				break;
+			case INTEGER:
+				rsPath=config.getInstance().getrsPathInteger();
+				rfPath=config.getInstance().getrfPathInteger();
+				break;
+			case BINARY:
+				rsPath=config.getInstance().getrsPathBinary();
+				rfPath=config.getInstance().getrfPathBinary();
+				break;
+		}
 		
-		  /**
-		   * 
-		   * This method generates a dataset from the list that contains the results
-		   * @return dataset
-		   */
-		   private DefaultCategoryDataset createDataset( UserProblem problem) {
-		
-			   DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-			   for(int x=0; x<xAxis.size();x++) {
-				   double[] yAxis = xAxis.get(x);
-				   
-				   for(int y=0; y<yAxis.length;y++) {
-					   if(y<problem.getCriterias().size())
-						   dataset.addValue(yAxis[y], "Variable "+x, problem.getCriterias().get(y).getName());
-					   else
-						   dataset.addValue(yAxis[y], "Variable "+x, "Criteria "+y);
-				   }
-			   }
-			   
-		      return dataset;
-		   }
-		
-		 		   
-		   /**
-		    * This method reads JMetal results from a file
-		    * @param path
-		    */
-		   public void readResults(String path) {
-				Scanner scanner = null;
-				
-				if(Paths.get(path).toFile().exists()) {
-				try{
-							File file = new File(path);
-						scanner = new Scanner(file);
-						
-						while(scanner.hasNextLine()){
-							String line = scanner.nextLine();
-							String[] splitLine = line.split(" ");
-							double[] yAxis = new double[splitLine.length];
-							
-							for(int i=0;i<splitLine.length;i++) {
-								if(!splitLine[i].equals("X"))
-									yAxis[i] = Double.parseDouble(splitLine[i]);
-							}
-							xAxis.add(yAxis);
-							
-						
-						}
-					}catch(FileNotFoundException e){
-							e.printStackTrace();
-					}finally{
-							scanner.close();
-					}
+		File rsFolder = new File(rsPath);
+		File[] listOfrs = rsFolder.listFiles();
+
+		for (int i = 0; i < listOfrs.length; i++) {
+			File file = listOfrs[i];
+			if (file.isFile() && file.getName().endsWith(".rs") 
+				&& file.getName().toUpperCase().contains(problem.getType().toString()) &&
+					containsAlgorithm(file.getName())) {
+					readResults(file.getPath());
+					setContent(file.getName());
 				}
-				
-		   }
+		}
+		
+		
+		File rfFolder = new File(rfPath);
+		File[] listOfrf = rfFolder.listFiles();
+
+		for (int i = 0; i < listOfrf.length; i++) {
+			File file = listOfrf[i];
+			if (file.isFile() && file.getName().endsWith(".rf") 
+					&& file.getName().toUpperCase().contains(problem.getType().toString()) &&
+			containsAlgorithm(file.getName())) {
+				readResults(file.getPath());
+				setContent(file.getName());
+			}
 		}
 
+	}
 
+	public boolean containsAlgorithm(String filename) {
+		for(String algorithm : algorithmsSelectedList) {
+			if(algorithm.contains(filename)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * This method sets the panel content of the graphic with the results
+	 */
+	public void setContent(String title) {
+		JFrame algorithmFrame = new JFrame();
+		JPanel pnlChart = new JPanel();
+		JFreeChart lineChart = ChartFactory.createLineChart(title, "", "", createDataset(problem),
+				PlotOrientation.VERTICAL, true, true, false);
+
+		ChartPanel chartPanel = new ChartPanel(lineChart);
+		chartPanel.setPreferredSize(new java.awt.Dimension((int) width, (int) height));
+		pnlChart.add(chartPanel);
+		algorithmFrame.add(pnlChart);
+		algorithmFrame.pack();
+		RefineryUtilities.centerFrameOnScreen(algorithmFrame);
+		algorithmFrame.setVisible(true);
+
+	}
+
+	/**
+	 * 
+	 * This method generates a dataset from the list that contains the results
+	 * 
+	 * @return dataset
+	 */
+	private DefaultCategoryDataset createDataset(UserProblem problem) {
+
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		for (int x = 0; x < xAxis.size(); x++) {
+			double[] yAxis = xAxis.get(x);
+
+			for (int y = 0; y < yAxis.length; y++) {
+				if (y < problem.getCriterias().size())
+					dataset.addValue(yAxis[y], "Variable " + x, problem.getCriterias().get(y).getName());
+				else
+					dataset.addValue(yAxis[y], "Variable " + x, "Criteria " + y);
+			}
+		}
+
+		return dataset;
+	}
+
+	/**
+	 * This method reads JMetal results from a file
+	 * 
+	 * @param path
+	 */
+	public void readResults(String path) {
+		Scanner scanner = null;
+
+		if (Paths.get(path).toFile().exists()) {
+			try {
+				File file = new File(path);
+				scanner = new Scanner(file);
+
+				while (scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					String[] splitLine = line.split(" ");
+					double[] yAxis = new double[splitLine.length];
+
+					for (int i = 0; i < splitLine.length; i++) {
+						if (!splitLine[i].equals("X"))
+							yAxis[i] = Double.parseDouble(splitLine[i]);
+					}
+					xAxis.add(yAxis);
+
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} finally {
+				scanner.close();
+			}
+		}
+
+	}
+}
