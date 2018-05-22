3e5a0d90e12ce390126cc40abaec588560d60061
Ricardo Santos <ricardojoaosantos0@gmail.com>
2018-05-22 20:35:58 +0100

    Email with attachement

--- src/support/EmailHandler.java
+++ src/support/EmailHandler.java
@@ -2,11 +2,22 @@ package support;
 
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.jfree.chart.axis.SubCategoryAxis;
 
 import java.security.Security;
 import com.sun.mail.smtp.SMTPTransport;
@@ -14,6 +25,8 @@ import java.util.Date;
 import javax.mail.internet.AddressException;
 
 public class EmailHandler {
+	
+	private Config config = Config.getInstance();
 	
 	public EmailHandler() {
 		
@@ -29,7 +42,7 @@ public class EmailHandler {
      * @throws AddressException if the email address parse failed
      * @throws MessagingException if the connection is dead or not in the connected state or if the message is not a MimeMessage
      */
-    public void sendEmail(String fromEmail, String ToEmail ,String ccEmail  ,String title, String message) throws AddressException, MessagingException {
+    public void sendEmail(String fromEmail, String ToEmail ,String ccEmail  ,String title, String message, boolean attach) throws AddressException, MessagingException {
         Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
         
@@ -72,11 +85,68 @@ public class EmailHandler {
 
         SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
 
-        t.connect("smtp.gmail.com", "projetodees7", "P@ssw0rd123");
+        t.connect("smtp.gmail.com",config.getEmailAdmin().split("@")[0],config.getEmailPassword());
         t.sendMessage(msg, msg.getAllRecipients());      
         t.close();
     }
+    
+    public void sendEmailWithAttachment(String fromEmail, String toEmail ,String ccEmail  ,String title, String messageBody, boolean attach) {
 
-}
+        final String username = config.getEmailAdmin();
+        final String password = config.getEmailPassword();
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail));
+            message.setSubject(title);
+            message.setText(messageBody);
+            
+            if (!ccEmail.equals("none")) {
+            	message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+            }
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+            Multipart multipart = new MimeMultipart();
+
+            messageBodyPart = new MimeBodyPart();
+            String file = "./tmp_240424042404.xml";
+            String fileName = "problemXML";
+            DataSource source = new FileDataSource(file);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(fileName);
+            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
+
+            System.out.println("Sending");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            System.out.println("Problem sending email");
+        }
+     }
+    }
+
+
 
 
