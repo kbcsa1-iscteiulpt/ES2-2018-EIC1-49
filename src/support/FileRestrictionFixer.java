7ac28f97d88542ceb75d21f8b7b6eafabcdca51a
kevin <kbcsa1@iscte-iul.pt>
2018-05-25 00:41:33 +0100

    FileRestrictionFixer updated

--- src/support/FileRestrictionFixer.java
+++ src/support/FileRestrictionFixer.java
@@ -17,24 +17,29 @@ public class FileRestrictionFixer {
 		String[] paths = new String[]{config.getResultsPathBinary(),config.getResultsPathDouble(),
 			config.getResultsPathInteger()};
 		
+		String oldFullPath="";
 		for(String path : paths) {
-			File dir = new File("./"+config.getExperimentBaseDirectory()+"/"+path);
-		    File[] directoryListing = dir.listFiles();
-		    if (directoryListing != null) {
-		      for (File file : directoryListing) {
-			    	  if(file.getName().endsWith(".rs")) {
-			    		  try (BufferedReader reader = new BufferedReader(new FileReader(file)); FileWriter writer = new FileWriter(file);) {
-			    		        String line;
-			    		        while ((line = reader.readLine()) != null) {
-			    		           String variables;
-			    		           for(int i=0; i<nLines;i++) {
-			    		        	   
-			    		           }
-			    		        }
-			    		    }
-			    	  }
-		      }
-		    } 
+			String fullPath="./"+config.getExperimentBaseDirectory()+"/"+path;
+			if(oldFullPath.equals(fullPath)) {
+				File dir = new File(fullPath);
+			    File[] directoryListing = dir.listFiles();
+			    if (directoryListing != null) {
+			      for (File file : directoryListing) {
+				    	  if(file.getName().endsWith(".rs")) {
+				    		  try (BufferedReader reader = new BufferedReader(new FileReader(file)); FileWriter writer = new FileWriter(file);) {
+				    		        String line;
+				    		        while ((line = reader.readLine()) != null) {
+				    		           String variables;
+				    		           for(int i=0; i<nLines;i++) {
+				    		        	   
+				    		           }
+				    		        }
+				    		    }
+				    	  }
+			      }
+			    } 
+			}
+		    oldFullPath = fullPath;
 		}
 	}
 	
